22d3f5a3059e68337f94cec6347502498d1b6d9e
Dulal-Mondal <dm640183@gmail.com>
2025-09-20 00:04:04 +0600

    visited country are displaied

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries }) => {
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+
+        // if (visited) {
+        //     setVisited(false);
+        // }
+        // else {
+        //     setVisited(true);
+        // }
+        // setVisited(visited ? false : true);
+        setVisited(!visited);
+        handleVisitedCountries(country)
+
+    }
     return (
-        <div className='country-card'>
+        <div className={`country-card ${visited && 'country-visited'}`}>
             <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
             <h3>Name:{country.name.common}</h3>
 
             <h3>Populations:{country.population.population}</h3>
-            <h3>Area:{country.area.area} sq.km</h3>
+            <h3>Area:{country.area.area} sq.km  {country.area.area > 300000 ? "large country" : "small country"}</h3>
+
+            <button onClick={handleVisited}>
+                {visited ? "Visites" : "Not visited"}
+            </button>
         </div>
     );
 };
